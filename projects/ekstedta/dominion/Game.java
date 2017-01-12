93c01a834e64f3a1a2e86d5beb11ce01fc42ec58
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2017-01-11 18:51:26 -0800

    ekstedta/dominion: stub out remaining Game methods
    Signatures and comments adapted from the C dominion source code.

--- projects/ekstedta/dominion/Game.java
+++ projects/ekstedta/dominion/Game.java
@@ -40,4 +40,44 @@ public class Game {
             this.discard.set(i, new ArrayList<Card>());
         }
     }
+
+    // Shuffle a player's deck.
+    // Assumes all cards are now in deck array (or hand/played);
+    // aftewards, discard is empty
+    public void shuffle(int player) {}
+
+    // Play card with index handPos from current player's hand
+    public void playCard(int handPos, int choice1, int choice2, int choice3) {}
+
+    // Buy card with supply index supplyPos
+    public void buyCard(int supplyPos) {}
+
+    // How many cards current player has in hand
+    public int numHandCards() { return 0; }
+
+    // enum value of indexed card in player's hand
+    public Card handCard(int handNum) { return Card.Copper; }
+
+    // How many of given card are left in supply
+    public int supplyCount(Card card) { return 0; }
+
+    // Count how many cards of a certain type a player has, in total
+    public int fullDeckCount(int player, Card card) { return 0; }
+
+    // Get the current player
+    public int whoseTurn() { return 0; }
+
+    // End the current player's turn
+    // Must do phase C and advance to next player;
+    // do not advance whose turn if game is over
+    public void endTurn() {}
+
+    public boolean isGameOver() { return false; }
+
+    // Scores may be negative
+    public int scoreFor(int player) { return 0; }
+
+    // Set array position of each player who won (remember ties!) to 1, others to 0
+    public List<Integer> getWinners() { return new ArrayList<Integer>(); }
+
 }
