14ff94fc7f3aab9acad37710e5f534873444804a
Connor Christensen <chriconn@oregonstate.edu>
2017-01-31 14:08:50 -0800

    moved all player comments and documentation to top of file

--- projects/chriconn/Player.java
+++ projects/chriconn/Player.java
@@ -1,25 +1,96 @@
+/*
+Class Player
+    Variables:
+        Private:
+            Deck drawDeck
+            Deck hand
+            Deck discard
+
+            String name
+            int actions
+            int value
+            int buys
+
+    Functions:
+        Private:
+
+        Public:
+            void    showHand()
+            void    playCard(String)
+            void    buy()
+            void    draw()
+            void    discard()
+            void    draw(int)
+            void    recycle()
+            String  getName()
+            int     getActions()
+            int     getValues()
+            int     getBuys()
+            boolean hasActions()
+            boolean hasMoves()
+
+
+        ShowHand:
+
+
+        PlayCard:
+
+
+        Buy:
+
+
+        Draw:
+
+
+        Discard:
+
+
+        Draw:
+            Description: draws a certain number of cards and adds it to hand
+            Input:  number of cards to draw
+            Output: none
+            Potential Errors:
+                - Draws more cards than are in the drawDeck
+                - Number exceeds the number of cards in the deck
+                - Number is negative
+
+        Recycle:
+
+
+        GetName:
+
+
+        GetActions:
+
+
+        GetValues:
+
+
+        GetBuys:
+
+
+        HasActions:
+
+
+        HasMoves:
+
+*/
+
 public class Player {
 
-    Deck drawDeck;
-    Deck hand;
-    Deck discard;
+    private Deck drawDeck;
+    private Deck hand;
+    private Deck discard;
 
     //the name of the player
-    String name;
+    private String name;
     //number of actions the player has left in the turn
-    int actions;
+    private int actions;
     //purchasing power of the players hand
-    int value;
+    private int value;
     //how many buys you can use
-    int buys;
+    private int buys;
 
-    /*
-        Description: -
-        Input:  -
-        Output: -
-        Potential Errors:
-            -
-    */
     public Player() {
         //build the starter deck
         drawDeck = new Deck("starter");
@@ -37,23 +108,10 @@ public class Player {
         drawDeck.shuffle();
     }
 
-    /*
-        Description: -
-        Input:  -
-        Output: -
-        Potential Errors:
-            -
-    */
     public Player(String playerName) {
-        //build the starter deck
         drawDeck = new Deck("starter");
-        //build an empty hand
         hand = new Deck();
-        //build and empty discard pile
         discard = new Deck();
-
-        //both the actions of the player start with 0
-        //the game should add and remove action ammounts
         actions = 0;
         value = 0;
         buys = 0;
@@ -61,46 +119,18 @@ public class Player {
         drawDeck.shuffle();
     }
 
-    /*
-        Description: -
-        Input:  -
-        Output: -
-        Potential Errors:
-            -
-    */
     public void showHand() {
         hand.printDeck();
     }
 
-    /*
-        Description: -
-        Input:  -
-        Output: -
-        Potential Errors:
-            -
-    */
     public void playCard(String cardName) {
 
     }
 
-    /*
-        Description: -
-        Input:  -
-        Output: -
-        Potential Errors:
-            -
-    */
     public void buy() {
 
     }
 
-    /*
-        Description: -
-        Input:  -
-        Output: -
-        Potential Errors:
-            - Tries to draw a card when there are no more
-    */
     public void draw() {
         if (!drawDeck.empty()) {
             //the hand is drawing a card from the drawCard deck
@@ -108,13 +138,6 @@ public class Player {
         }
     }
 
-    /*
-        Description: -
-        Input:  -
-        Output: -
-        Potential Errors:
-            -
-    */
     public void discard() {
         if (!hand.empty()) {
             //the discard deck is drawing from the hand
@@ -122,28 +145,12 @@ public class Player {
         }
     }
 
-    /*
-        Description: draws a certain number of cards and adds it to hand
-        Input:  number of cards to draw
-        Output: none
-        Potential Errors:
-            - Draws more cards than are in the drawDeck
-            - Number exceeds the number of cards in the deck
-            - Number is negative
-    */
     public void draw(int number) {
         for (int x = 0; x < number; x++) {
             draw();
         }
     }
 
-    /*
-        Description: -
-        Input:  -
-        Output: -
-        Potential Errors:
-            -
-    */
     public void recycle() {
 
     }
