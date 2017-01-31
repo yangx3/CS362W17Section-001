510dc278df87eb5579fef862caf24c293429c188
Connor Christensen <chriconn@oregonstate.edu>
2017-01-31 15:03:29 -0800

    added default contstructor that builds the bank of cards and prints the bank

--- projects/chriconn/Game.java
+++ projects/chriconn/Game.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class Game {
     public static void main(String[] args) {
@@ -8,10 +9,10 @@ public class Game {
         Player connor = new Player("Connor");
 
         /*
-            Example of getting the user for inut
-        // Reading from System.in
+        Example of getting the user for inut
+        Reading from System.in
         Scanner reader = new Scanner(System.in);
-        // Scans the next token of the input as an int.
+        Scans the next token of the input as an int.
         int n = reader.nextInt();
         */
 
@@ -19,18 +20,45 @@ public class Game {
         System.out.println("Player "+connor.getName());
         connor.showHand();
 
-        System.out.println("\n\n");
-        connor.hand.addCard(new Card("smithy"));
+        Game dominion = new Game();
+        System.out.println();
+        dominion.printBank();
 
+    }
 
-        // connor.hand.printType("action");
+    //array of all decks the game owns
+    ArrayList<Deck> bank;
+    int numPlayers;
 
+    public Game() {
+        bank = new ArrayList<Deck>();
+        //treasure
+        bank.add(new Deck(30, new Card("gold")));
+        bank.add(new Deck(40, new Card("silver")));
+        bank.add(new Deck(60, new Card("copper")));
 
-        connor.hand.addCard(new Card("gold"));
-        connor.showHand();
-        // while (connor.hasActions()) {
-        //
-        // }
+        //victory
+        bank.add(new Deck(12, new Card("province")));
+        bank.add(new Deck(12, new Card("dutchy")));
+        bank.add(new Deck(24, new Card("estate")));
+
+        //action
+        bank.add(new Deck(10, new Card("village")));
+        bank.add(new Deck(10, new Card("smithy")));
+        bank.add(new Deck(10, new Card("adventurer")));
+        bank.add(new Deck(10, new Card("curse")));
+        bank.add(new Deck(10, new Card("witch")));
+        bank.add(new Deck(10, new Card("cellar")));
+        bank.add(new Deck(10, new Card("market")));
+    }
+
+    public void printBank() {
+        System.out.println("Bank:");
+        System.out.println("Number of decks left: " + bank.size());
+        System.out.println("Number of decks empty: " + (13-bank.size()));
+        for (int x = 0; x < bank.size(); x++) {
+            System.out.printf("Deck Type: %-20s# in deck: %d\n", bank.get(x).cardInfo(0).getName(), bank.get(x).numCards());
+        }
     }
 }
 
