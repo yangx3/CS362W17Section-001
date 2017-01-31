1e74d8930e3ae3bdfe9392f83b1419c80df6e460
Connor Christensen <chriconn@oregonstate.edu>
2017-01-31 15:14:20 -0800

    added a non-default game contructor that takes any number of names and builds those players

--- projects/chriconn/Game.java
+++ projects/chriconn/Game.java
@@ -20,7 +20,7 @@ public class Game {
         System.out.println("Player "+connor.getName());
         connor.showHand();
 
-        Game dominion = new Game();
+        Game dominion = new Game("Connor", "Billy", "Lily");
         System.out.println();
         dominion.printBank();
 
@@ -29,9 +29,11 @@ public class Game {
     //array of all decks the game owns
     ArrayList<Deck> bank;
     int numPlayers;
+    ArrayList<Player> players;
 
     public Game() {
         bank = new ArrayList<Deck>();
+        players = new ArrayList<Player>();
         //treasure
         bank.add(new Deck(30, new Card("gold")));
         bank.add(new Deck(40, new Card("silver")));
@@ -52,6 +54,33 @@ public class Game {
         bank.add(new Deck(10, new Card("market")));
     }
 
+    public Game(String ... names) {
+        bank = new ArrayList<Deck>();
+        //treasure
+        bank.add(new Deck(30, new Card("gold")));
+        bank.add(new Deck(40, new Card("silver")));
+        bank.add(new Deck(60, new Card("copper")));
+
+        //victory
+        bank.add(new Deck(12, new Card("province")));
+        bank.add(new Deck(12, new Card("dutchy")));
+        bank.add(new Deck(24, new Card("estate")));
+
+        //action
+        bank.add(new Deck(10, new Card("village")));
+        bank.add(new Deck(10, new Card("smithy")));
+        bank.add(new Deck(10, new Card("adventurer")));
+        bank.add(new Deck(10, new Card("curse")));
+        bank.add(new Deck(10, new Card("witch")));
+        bank.add(new Deck(10, new Card("cellar")));
+        bank.add(new Deck(10, new Card("market")));
+
+        players = new ArrayList<Player>();
+        for (String s: names) {
+            players.add(new Player(s));
+        }
+    }
+
     public void printBank() {
         System.out.println("Bank:");
         System.out.println("Number of decks left: " + bank.size());
@@ -60,6 +89,8 @@ public class Game {
             System.out.printf("Deck Type: %-20s# in deck: %d\n", bank.get(x).cardInfo(0).getName(), bank.get(x).numCards());
         }
     }
+
+
 }
 
 
