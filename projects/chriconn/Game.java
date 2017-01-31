eab2139391cad08a4b2d4d5ee18d7ee9bc4773d2
Connor Christensen <chriconn@oregonstate.edu>
2017-01-31 00:28:24 -0800

    progresively more testing in the game function for the new functions added in the other classes

--- projects/chriconn/Game.java
+++ projects/chriconn/Game.java
@@ -2,7 +2,35 @@ import java.io.*;
 
 public class Game {
     public static void main(String[] args) {
-        Player connor = new Player();
+        //clear the screen
+        System.out.print("\033[2J\033[K\033[H");
+
+        Player connor = new Player("Connor");
+
+        /*
+            Example of getting the user for inut
+        // Reading from System.in
+        Scanner reader = new Scanner(System.in);
+        // Scans the next token of the input as an int.
+        int n = reader.nextInt();
+        */
+
+        connor.draw(5);
+        System.out.println("Player "+connor.getName());
+        connor.showHand();
+
+        System.out.println("\n\n");
+        connor.hand.addCard(new Card("smithy"));
+
+
+        // connor.hand.printType("action");
+
+
+        connor.hand.addCard(new Card("gold"));
+        connor.showHand();
+        // while (connor.hasActions()) {
+        //
+        // }
     }
 }
 
