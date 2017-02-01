b9e5e1e358e17f96776c1bdfe3c86236a7a947f0
Robert Rosenberger <rosenber@oregonstate.edu>
2017-01-31 19:07:22 -0800

    Declaration of Player class as well as header comment

---
+++ projects/rosenber/Player.java
@@ -0,0 +1,16 @@
+/*
+ * This class implements the functionality a player needs for a game
+ * of Dominion. The player has full control over all of the cards in its
+ * possession (hand, deck, etc). This puts all of the responsibility for
+ * error handling on the player. The game must ask the player what they
+ * would like to do, and the player will give. They are not responsible
+ * for making sure their action is valid for the point in the game, only
+ * for accessing the places in their hand and decks.
+ * 
+ * Robert Rosenberger
+ * Last modified: 1/31/2017
+ */
+
+public class Player{
+	
+}
