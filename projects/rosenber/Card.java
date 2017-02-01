dd8d0b60c75057d5a900a0f2f6cc4d0320f9c8a0
Robert Rosenberger <rosenber@oregonstate.edu>
2017-01-31 19:00:27 -0800

    Created class delcaration and header comments

---
+++ projects/rosenber/Card.java
@@ -0,0 +1,22 @@
+/*
+ * This class will implement a Super Card class for the cards in a game of
+ * Dominion. The cards will hold all of the information that every card in
+ * the game has. There are functions that will implement the specific
+ * features of each card. If a card does not have one of those features,
+ * the function should not be changed in the inherited class.
+ * 	Example:
+ *   - Dutchy has no action, it will not change the action function
+ *   - A copper is worth no victory points, it will have 0 victory points
+ *   - Etc
+ *   
+ *   Robert Rosenberger
+ *   Last modified: 1/31/2017
+ */
+
+public class Card {
+	
+}
+
+
+
+
