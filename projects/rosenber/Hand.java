764d96583776683798ddc225879564a25a2a6bc3
Robert Rosenberger <rosenber@oregonstate.edu>
2017-01-31 19:04:20 -0800

    Class declaration for Hand as well as header comment

---
+++ projects/rosenber/Hand.java
@@ -0,0 +1,11 @@
+/*
+ * This class implements a hand of cards for a game of Dominion. Cards
+ * can be added, removed, played, etc.
+ * 
+ * Robert Rosenberger
+ * Last modified: 1/31/2017
+ */
+
+public class Hand{
+	
+}
