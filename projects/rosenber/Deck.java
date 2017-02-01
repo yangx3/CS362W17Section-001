79f7048820b2d652758532d7b99aa6a9108b85a2
Robert Rosenberger <rosenber@oregonstate.edu>
2017-01-31 19:03:33 -0800

    Class declaration for Deck as well as header comment

---
+++ projects/rosenber/Deck.java
@@ -0,0 +1,12 @@
+/*
+ * This class holds all of the Cards in a Dominion deck as well as
+ * implements all of the functionality it needs. Cards can be added,
+ * taken, viewed, etc from the deck.
+ * 
+ *  Robert Rosenberger
+ *  Last modified: 1/31/2017
+ */
+
+public class Deck{
+	
+}
