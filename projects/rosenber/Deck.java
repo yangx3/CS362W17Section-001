2781a937c02faa70d82e25031f779da94471d52f
Robert Rosenberger <rosenber@oregonstate.edu>
2017-01-31 19:34:59 -0800

    Created function prototypes in Deck

--- projects/rosenber/Deck.java
+++ projects/rosenber/Deck.java
@@ -6,7 +6,116 @@
  *  Robert Rosenberger
  *  Last modified: 1/31/2017
  */
+import java.util.ArrayList;
 
 public class Deck{
 	
+	private ArrayList<Card> deck;
+	
+	//constructor
+	
+	//add card to the top
+	public void addTop(Card c){
+		
+	}
+	
+	//add card to the bottom
+	public void addBottom(Card c){
+		
+	}
+	
+	//returns the card at the top
+	public Card getTop(){
+		
+	}
+	
+	//returns the card at the bottom
+	public Card getBottom(){
+		
+	}
+	
+	//remove card from the top
+	public void removeTop(){
+		
+	}
+	
+	//remove card from the bottom
+	public void removeBottom(){
+		
+	}
+	
+	//removes and returns the card at the top
+	public Card drawTop(){
+		
+	}
+	
+	//removes and returns the card at the bottom
+	public Card drawBottom(){
+		
+	}
+	
+	//swaps cards at given indices
+	// - a and b must be good
+	private void swap(int a, int b){
+		
+	}
+	
+	//shuffles the deck
+	public void shuffle(){
+		
+	}
+	
+	//checks to see if a specific card is in the deck
+	// - returns true if it is, false otherwise
+	public boolean isCard(Card c){
+		
+	}
+	
+	//returns a specific card from the deck
+	// - returns null if it isn't
+	public Card findSpecific(Card c){
+		
+	}
+	
+	//removes a specific card from the deck
+	//does nothing if it's not there
+	public void removeSpecific(Card c){
+		
+	}
+	
+	//returns and removes a specific card from the deck
+	public Card getSpecific(Card c){
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
