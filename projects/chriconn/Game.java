f9931673de454d63c93b9989490d690dc1fb2a5a
Connor Christensen <chriconn@oregonstate.edu>
2017-02-02 01:52:59 -0800

    build more concise deck building structure

--- projects/chriconn/Game.java
+++ projects/chriconn/Game.java
@@ -57,6 +57,22 @@ Class Game
             Potential Errors:
                 -
 
+
+        Number of cards:
+        Gold:   30
+        Silver: 40
+        Copper: 60
+
+        Actions: 10
+
+        Estate:     24
+        Dutchy:     12
+        Province:   12
+
+        Curse:
+        2 players: 10
+        3 players: 20
+        4 players: 30
 */
 
 public class Game {
@@ -82,13 +98,19 @@ public class Game {
         //prints the whole bank for testing
         dominion.printBank();
 
-        for (int x = 0; x < 3; x++) {
-            dominion.playerTurn(x);
+        for (int x = 0; x < 20; x++) {
+            dominion.playerTurn(0);
         }
+
+
+        // for (int x = 0; x < 3; x++) {
+        //     dominion.playerTurn(x);
+        // }
     }
 
     //array of all decks the game owns
     ArrayList<Deck> bank;
+    ArrayList<Deck> trash;
     int numPlayers;
     ArrayList<Player> players;
     static int printTime;
@@ -116,13 +138,19 @@ public class Game {
         bank.add(new Deck(24, new Card("estate")));
 
         //action
-        bank.add(new Deck(10, new Card("village")));
-        bank.add(new Deck(10, new Card("smithy")));
-        bank.add(new Deck(10, new Card("adventurer")));
-        bank.add(new Deck(10, new Card("curse")));
-        bank.add(new Deck(10, new Card("witch")));
-        bank.add(new Deck(10, new Card("cellar")));
-        bank.add(new Deck(10, new Card("market")));
+        // bank.add(new Deck(10, new Card("village")));
+        // bank.add(new Deck(10, new Card("smithy")));
+        // bank.add(new Deck(10, new Card("adventurer")));
+        // bank.add(new Deck(10, new Card("curse")));
+        // bank.add(new Deck(10, new Card("witch")));
+        // bank.add(new Deck(10, new Card("cellar")));
+        // bank.add(new Deck(10, new Card("market")));
+
+        String[] decksToImplement = {"adventurer", "ambassador", "baron", "council room", "cutpurse", "embargo", "feast", "gardens", "great hall", "mine", "curse"};
+
+        for (String deckName: decksToImplement) {
+            bank.add(new Deck(10, new Card(deckName)));
+        }
     }
 
     public Game(String ... names) {
@@ -138,14 +166,21 @@ public class Game {
         bank.add(new Deck(24, new Card("estate")));
 
         //action
-        bank.add(new Deck(10, new Card("village")));
-        bank.add(new Deck(10, new Card("smithy")));
-        bank.add(new Deck(10, new Card("adventurer")));
-        bank.add(new Deck(10, new Card("curse")));
-        bank.add(new Deck(10, new Card("witch")));
-        bank.add(new Deck(10, new Card("cellar")));
-        bank.add(new Deck(10, new Card("market")));
+        // bank.add(new Deck(10, new Card("village")));
+        // bank.add(new Deck(10, new Card("smithy")));
+        // bank.add(new Deck(10, new Card("adventurer")));
+        // bank.add(new Deck(10, new Card("curse")));
+        // bank.add(new Deck(10, new Card("witch")));
+        // bank.add(new Deck(10, new Card("cellar")));
+        // bank.add(new Deck(10, new Card("market")));
 
+        //build the action decks
+        String[] decksToImplement = {"adventurer", "ambassador", "baron", "council room", "cutpurse", "embargo", "feast", "gardens", "great hall", "mine", "curse", "smithy", "village"};
+        for (String deckName: decksToImplement) {
+            bank.add(new Deck(10, new Card(deckName)));
+        }
+
+        //build the players
         players = new ArrayList<Player>();
         for (String s: names) {
             players.add(new Player(s));
@@ -153,67 +188,68 @@ public class Game {
         numPlayers = names.length;
     }
 
-    public void printBank() {
-        System.out.print("Number of decks left: " + bank.size());
-        System.out.println("\tNumber of decks empty: " + (13-bank.size()));
-        for (int x = 0; x < bank.size(); x++) {
-            System.out.printf("%-15s# remaining: %d\tCost: %d\n", bank.get(x).cardInfo(0).getName(), bank.get(x).numCards(), bank.get(x).cardInfo(0).getCost());
-        }
-    }
-
-    public void printBank(int coinLimit) {
-        System.out.print("Number of decks left: " + bank.size());
-        System.out.println("\tNumber of decks empty: " + (13-bank.size()));
-        for (int x = 0; x < bank.size(); x++) {
-            if (bank.get(x).cardInfo(0).getCost() <= coinLimit) {
-                System.out.printf("%-15s# remaining: %d\tCost: %d\n", bank.get(x).cardInfo(0).getName(), bank.get(x).numCards(), bank.get(x).cardInfo(0).getCost());
-            }
-        }
-    }
-
     public void playerTurn(int num) {
-        /***** Variable initialization *****/
 
-        //players name
-        String name = getPlayer(num).getName();
-        //card the user will want to play
-        String cardToPlay;
+        /*******************************************************/
+        /*************** Variable initialization ***************/
+        /*******************************************************/
+
+        String name = getPlayer(num).getName(); //players name
+        String cardToPlay;                      //card the user will want to play
 
         //award the player one action and one buy to start the turn
         getPlayer(num).starterPoints();
 
-        /***** Begin actions *****/
+
+        /*******************************************************/
+        /******************** Begin actions ********************/
+        /*******************************************************/
 
         clearScreen();
-        //alert the player that the game will draw them 5 cards
         printLineDelay(name + ", the game is drawing 5 cards...");
-        //draw the 5 cards
         getPlayer(num).draw(5);
-        //clear the screen and print the cards in their hand
         clearAndShowHand(num, sleepTime);
 
-        /***** Action phase *****/
+        /*******************************************************/
+        /******************** Action phase ********************/
+        /*******************************************************/
 
         //if the player has action points and they have action cards in their hand
-        if (getPlayer(num).hasActions()) {
-            printLineDelay("Please play an action card: ");
+        while (getPlayer(num).hasActions()) {
+
+            printLineDelay("Please play an action card:\n");
             //print the number of actions, buys and coins the player has
             System.out.println(getPlayer(num).getMoves());
+
+            System.out.println("\nCards you can play:");
+            //print only the action cards in their hand
+            getPlayer(num).printHandType("action");
+            System.out.print("Select your card: ");
+
+            cardToPlay = scanner.nextLine();
+
+            getPlayer(num).playCard(cardToPlay);
+
+
+            if (cardToPlay.equals("skip")) {
+                getPlayer(num).skipActionsPhase();
+            }
+        }
+        //if player did not have an action card
+        if (!getPlayer(num).handContainsActions()) {
+            printLineDelay("You do not have an action card...");
         }
         else {
-            //if player did not have an action card
-            if (!getPlayer(num).handContainsActions()) {
-                printLineDelay("You do not have an action card...");
-            }
-            else {
-                printLineDelay("You do not have any more action points...");
-            }
-            printLineDelay("\nMoving on to buying phase...\n");
-            printLineDelay("Press enter to continue: ");
-            scanner.nextLine();
+            printLineDelay("You do not have any more action points...");
         }
+        printLineDelay("\nMoving on to buying phase...\n");
+        printLineDelay("Press enter to continue: ");
+        scanner.nextLine();
 
-        /***** Buying Phase *****/
+
+        /*******************************************************/
+        /******************** Buying Phase ********************/
+        /*******************************************************/
 
         //tally up the total treasure value
         getPlayer(num).sumTreasure();
@@ -238,14 +274,12 @@ public class Game {
             String purchaseCard = scanner.nextLine();
             purchaseCard = purchaseCard.toLowerCase();
 
-            //if the getDeck function couldn't find the deck, it will return null
-            //if it didn't return null, it means it found the deck.
-
             /*******#### HERE IS _BUG_ ####*******/
             /*******####
                 The buy function does not check the value of the card before buying
             ####*******/
-
+            //if the getDeck function couldn't find the deck, it will return null
+            //if it didn't return null, it means it found the deck.
             if (getDeck(purchaseCard) != null) {
                 getPlayer(num).buy(getDeck(purchaseCard));
                 System.out.println("Purchase sucessfull");
@@ -255,7 +289,7 @@ public class Game {
             else if (purchaseCard.equals("skip")) {
                 printLineDelay("Skipped. ");
                 printLineDelay("Your turn will now end...\n");
-                getPlayer(num).skipTurn();
+                getPlayer(num).endTurn();
                 done = true;
             }
             else {
@@ -275,6 +309,21 @@ public class Game {
             printBank();
             printLineDelay("Press enter to end your turn: ");
             scanner.nextLine();
+        }
+
+    }
+
+    public void printBank() {
+        for (int x = 0; x < bank.size(); x++) {
+            System.out.printf("%-15s# remaining: %d\tCost: %d\tDescription: %s\n", bank.get(x).cardInfo(0).getName(), bank.get(x).numCards(), bank.get(x).cardInfo(0).getCost(), bank.get(x).cardInfo(0).getDescription());
+        }
+    }
+
+    public void printBank(int coinLimit) {
+        for (int x = 0; x < bank.size(); x++) {
+            if (bank.get(x).cardInfo(0).getCost() <= coinLimit) {
+                System.out.printf("%-15s# remaining: %d\tCost: %d\tDescription: %s\n", bank.get(x).cardInfo(0).getName(), bank.get(x).numCards(), bank.get(x).cardInfo(0).getCost(), bank.get(x).cardInfo(0).getDescription());
+            }
         }
     }
 
@@ -312,9 +361,7 @@ public class Game {
     public void pause(int sleepTime) {
         try {
             Thread.sleep(sleepTime);
-        } catch (InterruptedException e) {
-            // e.printStackTrace();
-        }
+        } catch (InterruptedException e) {}
     }
 
     public void clearAndShowHand(int playerNumber, int sleepTime) {
@@ -343,26 +390,3 @@ public class Game {
         pause(sleepTime);
     }
 }
-
-
-
-
-
-
-/*
-    Number of cards:
-    Gold:   30
-    Silver: 40
-    Copper: 60
-
-    Actions: 10
-
-    Estate:     24
-    Dutchy:     12
-    Province:   12
-
-    Curse:
-    2 players: 10
-    3 players: 20
-    4 players: 30
-*/
