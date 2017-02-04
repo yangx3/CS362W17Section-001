63e7247aeec3fb7864f2ccd1ec7a6543f32861e0
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-03 17:58:57 -0800

    Finished implementing unit tests to get full code coverage

--- projects/rosenber/Dominion/src/test/java/org/cs362/dominion/BoardTest.java
+++ projects/rosenber/Dominion/src/test/java/org/cs362/dominion/BoardTest.java
@@ -24,6 +24,8 @@ public class BoardTest {
 				1, test.numDecks());
 		assertEquals("Initialzed deck in board isn't named 'Trash'",
 				"Trash", test.getName(0));
+		assertEquals("Initialzed deck in board isn't empty",
+				test.numCardsInDeck(0), 0);
 	}
 	
 	@Test
@@ -52,4 +54,25 @@ public class BoardTest {
 		}
 	}
 
+	@Test
+	public void testDeckInteraction(){
+		test = new Board();
+		Card c = createCard("one");
+		
+		test.addToTrash(c);
+		assertEquals("Trash doesn't have added card in it",
+				test.numCardsInDeck(0), 1);
+		
+		test.createDeck(createCard("two"), 5);
+		
+		Card drawn = test.draw(1);
+		assertFalse("Drawn card is equal to null",
+				drawn == null);
+		assertEquals("Deck didn't lose card after draw",
+				test.numCardsInDeck(1), 4);
+		assertEquals("Card doesn't match deck",
+				drawn.getName(), test.getName(1));
+		
+	}
+	
 }
