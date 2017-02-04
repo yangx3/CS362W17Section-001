7593f8f6b8ba94b3711cf1ec6865984ae50a77fe
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-03 17:17:44 -0800

    Created Board.java, wrote header comments and functions headers too

---
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Board.java
@@ -0,0 +1,40 @@
+/*
+ * This class implements the "board" needed for a game of Dominion. The class
+ * contains all of the decks needed to play the game. Players draw from these
+ * and the deck's cards get placed into the players control.
+ *  - The trash deck is always at idx 0
+ * 
+ * Robert Rosenberger
+ * Last Modified: 2/3/2017
+ */
+package org.cs362.dominion;
+import java.util.ArrayList;
+
+public class Board {
+	
+	ArrayList<Deck> decks;
+	
+	//constructor
+	
+	//create deck
+	// - takes in card to make deck out of
+	// - number cards that should be in the deck
+	
+	//add to deck
+	// - takes in card to add to the deck
+	
+	//add to trash
+	// - takes in card to add to trash deck
+	
+	//draw from deck, removes one card from the deck
+	// - takes idx of deck to draw from
+	// - returns card drawn
+	
+	//returns number of decks
+	
+	//get number of cards in deck
+	
+	//prints board
+	
+	
+}
