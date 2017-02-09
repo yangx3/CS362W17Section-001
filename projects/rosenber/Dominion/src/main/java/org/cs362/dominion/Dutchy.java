aaad0caf6a29f7d4a40fa8427a1eb675e2bc185c
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-08 23:07:03 -0800

    Created Dutchy class and made class header

---
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Dutchy.java
@@ -0,0 +1,16 @@
+/*
+ * Implements the Dutchy card for a game of Dominion. It
+ * extends the card class. Sets victory points to 3, and
+ * cost to 5.
+ * 
+ * Robert Rosenberger
+ * 2/8/2017
+ */
+
+package org.cs362.dominion;
+
+public class Dutchy extends Card {
+
+	
+	
+}
