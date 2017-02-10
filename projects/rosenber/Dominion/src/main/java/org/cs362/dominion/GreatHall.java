12a7c32f5528fd4a0d97d32aade3c1f3e1546924
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 23:26:18 -0800

    implementd constructor for greathall and mine

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/GreatHall.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/GreatHall.java
@@ -1,5 +1,10 @@
 package org.cs362.dominion;
 
-public class GreatHall {
+public class GreatHall extends Card {
 
+	public GreatHall(){
+		super("Great Hall", 3, 1, 0,
+				new CardType[] {CardType.Action, CardType.Victory});
+	}
+	
 }
--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Mine.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Mine.java
@@ -1,5 +1,10 @@
 package org.cs362.dominion;
 
-public class Mine {
+public class Mine extends Card {
 
+	public Mine(){
+		super("Mine", 5, 0, 0,
+				new CardType[] {CardType.Action});
+	}
+	
 }
