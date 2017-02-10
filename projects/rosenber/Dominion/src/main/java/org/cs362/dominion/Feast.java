76950063f013aee202032ab23e926849ec95c81d
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 23:22:01 -0800

    implementd constructor for feast

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Feast.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Feast.java
@@ -1,5 +1,10 @@
 package org.cs362.dominion;
 
-public class Feast {
+public class Feast extends Card {
 
+	public Feast(){
+		super("Feast", 4, 0, 0,
+				new CardType[] {CardType.Action});
+	}
+	
 }
