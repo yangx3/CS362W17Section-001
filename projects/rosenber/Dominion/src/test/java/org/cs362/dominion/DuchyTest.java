aa2969f07ece775be5fdc402acf0981b3bcd1a4a
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 22:41:36 -0800

    Created test files for these cards

---
+++ projects/rosenber/Dominion/src/test/java/org/cs362/dominion/DuchyTest.java
@@ -0,0 +1,14 @@
+package org.cs362.dominion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DuchyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ projects/rosenber/Dominion/src/test/java/org/cs362/dominion/SilverTest.java
@@ -0,0 +1,14 @@
+package org.cs362.dominion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SilverTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
