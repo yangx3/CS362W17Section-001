643af4ae8ec441a05cf5844104bebed91c3809a2
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 23:23:13 -0800

    implementd constructor for garden

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Garden.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Garden.java
@@ -1,5 +1,12 @@
 package org.cs362.dominion;
 
-public class Garden {
+public class Garden extends Card{
+	
+	public Garden(){
+		
+		super("Garden", 4, 0, 0,
+				new CardType[] {CardType.Victory});
+		
+	}
 
 }
