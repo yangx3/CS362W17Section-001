7ea27d11be1870c079f382ea7a907178a841aa79
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 23:19:59 -0800

    implementd constructor for cutpurse

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Cutpurse.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Cutpurse.java
@@ -1,5 +1,10 @@
 package org.cs362.dominion;
 
-public class Cutpurse {
+public class Cutpurse extends Card{
 
+	public Cutpurse(){
+		super("Cutpurse", 4, 0, 0,
+				new CardType[] {CardType.Action, CardType.Attack});
+	}
+	
 }
