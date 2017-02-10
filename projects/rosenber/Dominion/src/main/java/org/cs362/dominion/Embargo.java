cd53ac5138ca19f006fce6c40dc8d61e76012c5e
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 23:20:55 -0800

    implementd constructor for embargo

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Embargo.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Embargo.java
@@ -1,5 +1,10 @@
 package org.cs362.dominion;
 
-public class Embargo {
+public class Embargo extends Card{
 
+	public Embargo(){
+		super("Embargo", 2, 0, 0,
+				new CardType[] {CardType.Action});
+	}
+	
 }
