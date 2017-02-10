97f989e6cfaac12b7a87297b59e829e46fb20a39
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 23:55:59 -0800

    modified whitespace in adventurer card

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Adventurer.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Adventurer.java
@@ -3,7 +3,8 @@ package org.cs362.dominion;
 public class Adventurer extends Card {
 
 	public Adventurer(){
-		super("Adventurer", 6, 0, 0, new CardType[] {CardType.Action});
+		super("Adventurer", 6, 0, 0, 
+				new CardType[] {CardType.Action});
 	}
 	
 }
