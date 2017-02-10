4b6e2fde49c77b6a8d33d258793ce16043a74510
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 23:28:23 -0800

    implementd constructor for smithy

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Smithy.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Smithy.java
@@ -1,5 +1,10 @@
 package org.cs362.dominion;
 
-public class Smithy {
+public class Smithy extends Card {
 
+	public Smithy(){
+		super("Smithy", 3, 0, 0,
+				new CardType[] {CardType.Action});
+	}
+	
 }
