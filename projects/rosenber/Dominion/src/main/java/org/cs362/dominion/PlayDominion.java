3faaa87a99387ce9e06d8ca4006d532bd46812c7
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-10 09:38:06 -0800

    Created new class that is actually what plays the game of dominon and has the main()

---
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/PlayDominion.java
@@ -0,0 +1,15 @@
+/*
+ * This class is the actual Dominion playing class. It creates a game and
+ * asks if the user wants to replay or not
+ * 
+ * Robert Rosenberger
+ * 2/10/2017
+ */
+
+package org.cs362.dominion;
+
+public class PlayDominion {
+
+	Game game;
+	
+}
