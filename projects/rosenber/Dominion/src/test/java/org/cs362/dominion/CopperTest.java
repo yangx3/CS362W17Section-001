54dfd3a01ca3fae8195dc0243198d6996ce4c2c6
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 22:43:27 -0800

    Unit test for Copper card values

--- projects/rosenber/Dominion/src/test/java/org/cs362/dominion/CopperTest.java
+++ projects/rosenber/Dominion/src/test/java/org/cs362/dominion/CopperTest.java
@@ -8,7 +8,19 @@ public class CopperTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		Card test = new Copper();
+		assertEquals("Copper name doesn't match",
+				"Copper", test.getName());
+		assertEquals("Copper cost doesn't match",
+				0, test.getCost());
+		assertEquals("Copper victory points doesn't match",
+				0, test.getVictoryPoints());
+		assertEquals("Copper value doesn't match",
+				1, test.getTreasure());
+		assertTrue("Curse type doesn't match",
+				test.isCardType(CardType.Treasure));
+		
 	}
 
 }
