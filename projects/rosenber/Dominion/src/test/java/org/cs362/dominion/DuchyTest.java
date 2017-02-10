64bbb857d4a5e96a2ce079b577451b12a3048dab
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 22:45:36 -0800

    made check values test for duchy

--- projects/rosenber/Dominion/src/test/java/org/cs362/dominion/DuchyTest.java
+++ projects/rosenber/Dominion/src/test/java/org/cs362/dominion/DuchyTest.java
@@ -7,8 +7,20 @@ import org.junit.Test;
 public class DuchyTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testValues() {
+		
+		Card test = new Duchy();
+		assertEquals("Duchy name doesn't match",
+				"Duchy", test.getName());
+		assertEquals("Duchy cost doesn't match",
+				5, test.getCost());
+		assertEquals("Duchy victory points doesn't match",
+				3, test.getVictoryPoints());
+		assertEquals("Duchy value doesn't match",
+				0, test.getTreasure());
+		assertTrue("Duchy type doesn't match",
+				test.isCardType(CardType.Curse));
+		
 	}
 
 }
