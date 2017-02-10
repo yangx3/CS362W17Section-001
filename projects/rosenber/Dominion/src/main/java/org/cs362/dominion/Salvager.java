92c1ba6936430e8b523cdb1547aa2824dbb0ccc1
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 23:27:33 -0800

    implementd constructor for salvager

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Salvager.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Salvager.java
@@ -1,5 +1,10 @@
 package org.cs362.dominion;
 
-public class Salvager {
+public class Salvager extends Card{
 
+	public Salvager(){
+		super("Salvager", 4, 0, 0,
+				new CardType[] {CardType.Action});
+	}
+	
 }
