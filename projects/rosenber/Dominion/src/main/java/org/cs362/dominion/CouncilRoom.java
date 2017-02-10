57d8a5c6d513b70c7f3ef2f64902e4ae73b91d45
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 23:18:45 -0800

    implementd council room constructor

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/CouncilRoom.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/CouncilRoom.java
@@ -1,5 +1,10 @@
 package org.cs362.dominion;
 
-public class CouncilRoom {
+public class CouncilRoom extends Card{
 
+	public CouncilRoom(){
+		super("Council Room", 5, 0, 0,
+				new CardType[] {CardType.Action});
+	}
+	
 }
