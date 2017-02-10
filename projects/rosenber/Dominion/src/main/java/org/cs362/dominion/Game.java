6a8e6e0a02afe4591a1825450b1c5651821c0226
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-10 00:10:09 -0800

    Created game class with header comment

---
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Game.java
@@ -0,0 +1,14 @@
+/*
+ * This class implements the game of dominion to be played. All classes and objects
+ * are initialized in this class. This class facilitates the gameplay between the
+ * players and acts and the unspoken referee of Dominion.
+ * 
+ * Robert Rosenberger
+ * 2/10/2017
+ */
+
+package org.cs362.dominion;
+
+public class Game {
+
+}
