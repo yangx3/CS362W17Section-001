0e3d1e5cd58faa5f7d48754ba4fc433c4db265d5
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 23:12:43 -0800

    Created ambassador card and constructor

---
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Ambassador.java
@@ -0,0 +1,10 @@
+package org.cs362.dominion;
+
+public class Ambassador extends Card{
+	
+	public Ambassador(){
+		super("Ambassador", 3, 0, 0, 
+				new CardType[] {CardType.Action, CardType.Attack});
+	}
+	
+}
