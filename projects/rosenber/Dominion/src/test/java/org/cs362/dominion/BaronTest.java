4075c611a97287fb2ba68a309e6659d186b7c193
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-09 23:50:53 -0800

    Created test class and tested constructor for baron

---
+++ projects/rosenber/Dominion/src/test/java/org/cs362/dominion/BaronTest.java
@@ -0,0 +1,26 @@
+package org.cs362.dominion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BaronTest {
+
+	@Test
+	public void testValues() {
+		
+		Card test = new Baron();
+		assertEquals("Baron name doesn't match",
+				"Baron", test.getName());
+		assertEquals("Baron cost doesn't match",
+				5, test.getCost());
+		assertEquals("Baron victory points doesn't match",
+				0, test.getVictoryPoints());
+		assertEquals("Baron value doesn't match",
+				0, test.getTreasure());
+		assertTrue("Baron type doesn't match",
+				test.isCardType(CardType.Action));
+		
+	}
+
+}
