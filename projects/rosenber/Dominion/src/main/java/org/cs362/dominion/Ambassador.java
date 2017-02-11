1a86a21bf85893088b340ed270901392d6623cb4
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-11 10:10:10 -0800

    implemented ai version of action

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Ambassador.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Ambassador.java
@@ -2,6 +2,7 @@ package org.cs362.dominion;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Ambassador extends Card{
 	
@@ -18,6 +19,10 @@ public class Ambassador extends Card{
 	//performs the cards attack
 	public void Attack(ArrayList<Player> players,
 			Player currentPlayer, Board board){
+		if(currentPlayer.checkAi()){
+			AIAttack(players, currentPlayer, board);
+			return;
+		}
 		Scanner input = new Scanner(System.in);
 		boolean repeat;
 		Card choice = null;
@@ -51,4 +56,16 @@ public class Ambassador extends Card{
 		}while(repeat);
 		input.close();
 	};
+	public void AIAttack(ArrayList<Player> players, Player currentPlayer, Board board){
+		Random rand = new Random();
+		int choice = -1;
+		Card c = null;
+		choice = rand.nextInt(currentPlayer.numCardsHand());
+		c = currentPlayer.playCard(choice);
+		board.addToDeck(c, board.getDeckIdx(c.getName()));
+		for(int j=0; j<players.size(); j++)
+			if(players.get(j) != currentPlayer){
+				players.get(j).giveCard(board.draw(board.getDeckIdx(c.getName())));
+			}
+	}
 }
