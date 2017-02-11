55dcda374e8fd34808f516eabd4fe1784e2e0aa2
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-10 22:25:02 -0800

    implemented action for salvager

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Salvager.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Salvager.java
@@ -1,10 +1,35 @@
 package org.cs362.dominion;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Salvager extends Card{
 
 	public Salvager(){
 		super("Salvager", 4, 0, 0,
 				new CardType[] {CardType.Action});
 	}
-	
+	//performs the cards action
+	public void Action(ArrayList<Player> players,
+			Player currentPlayer, Board board){
+		currentPlayer.addBuys(1);
+		Card choice = null;
+		boolean repeat;
+		Scanner input = new Scanner(System.in);
+		do{
+			repeat = false;
+			System.out.println("What card would you like to trash?\n"
+					+ "You gain coins equal to its cost\n>");
+			String ans = input.nextLine();
+			try{
+				choice = currentPlayer.playCard(ans);
+				board.addToTrash(choice);
+				currentPlayer.addMoney(choice.getCost());
+			}
+			catch(Exception e){
+				System.out.println("That name isn't valid. Try again!");
+				repeat = true;
+			}
+		}while(repeat);
+	}
 }
