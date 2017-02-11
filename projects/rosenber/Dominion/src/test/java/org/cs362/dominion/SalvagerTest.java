c131831e1850770002bf9d0de47b89550ab9f9d7
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-11 11:13:53 -0800

    implemented unit test for action of salvager

--- projects/rosenber/Dominion/src/test/java/org/cs362/dominion/SalvagerTest.java
+++ projects/rosenber/Dominion/src/test/java/org/cs362/dominion/SalvagerTest.java
@@ -1,9 +1,9 @@
 package org.cs362.dominion;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 import java.util.ArrayList;
+
 public class SalvagerTest {
 
 	@Test
@@ -23,4 +23,39 @@ public class SalvagerTest {
 		
 	}
 
+	@Test
+	public void testAction(){
+		ArrayList<Player> players = new ArrayList<Player>();
+		Player currentPlayer = new AIPlayer(1);
+		Board board = new Board();
+		
+		currentPlayer.giveCard(new Salvager());
+		currentPlayer.drawCard();
+		
+		int trashSize = board.numCardsInDeck(0);
+		int handSize = currentPlayer.numCardsHand();
+		
+		Card c = new Salvager();
+		
+		c.Action(players, currentPlayer, board);
+		assertEquals("Card wasn't added to trash",
+				trashSize+1, board.numCardsInDeck(0));
+		assertEquals("Card wasn't removed from hand",
+				handSize-1, currentPlayer.numCardsHand());
+		assertEquals("Player didn't receive coins equals to value trashed",
+				4, currentPlayer.getMoney());
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
