c410f0bf4272f24935ae2a179e8fe056bd4535d9
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-11 11:43:47 -0800

    implemented unit test for action of ambassador

--- projects/rosenber/Dominion/src/test/java/org/cs362/dominion/AmbassadorTest.java
+++ projects/rosenber/Dominion/src/test/java/org/cs362/dominion/AmbassadorTest.java
@@ -2,6 +2,8 @@ package org.cs362.dominion;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class AmbassadorTest {
@@ -25,4 +27,31 @@ public class AmbassadorTest {
 		
 	}
 
+	@Test
+	public void testAction(){
+		ArrayList<Player> plist = new ArrayList<Player>();
+		Player player = new AIPlayer(1);
+		plist.add(player);
+		Board board = new Board();
+		
+		player.giveCard(new Copper());
+		player.drawCard();
+		
+		Player other = new AIPlayer(2);
+		plist.add(other);
+		board.createDeck(new Copper(), 0);
+		
+		Card c = new Ambassador();
+		
+		c.Action(plist, player, board);
+		other.drawCard();
+		
+		assertEquals("Deck should be empty",
+				0, board.numCardsInDeck(1));
+		assertEquals("Player didn't add its card to board",
+				0, player.numCardsHand());
+		assertEquals("Other player didn't gain copper",
+				1, other.numCardsHand());
+	}
+	
 }
