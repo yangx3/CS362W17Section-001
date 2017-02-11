b716f757d7bd77c2818a0fa92116f289f0d6a551
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-11 08:52:36 -0800

    added in embargo counters when buying cards

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Player.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Player.java
@@ -214,6 +214,11 @@ public class Player{
 				if(choice.getCost() >= money){
 					giveCard(board.draw(idx));
 					money -= choice.getCost();
+					if(choice.getName() == "Embargo"){
+						int numCurses = board.getDeck(board.getDeckIdx(choice.getName())).getEmbargos();
+						for(int j=0; j<numCurses; j++)
+							giveCard(board.draw(board.getDeckIdx("Curse")));
+					}
 				}
 				else{
 					System.out.println("You don't have enough money for that! Try again!");
