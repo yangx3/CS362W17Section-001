938ecc77a04225ca962fd9d54c1cfaea3e8e27b0
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-11 08:58:11 -0800

    Implemented action for embargo

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Embargo.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Embargo.java
@@ -1,10 +1,33 @@
 package org.cs362.dominion;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Embargo extends Card{
 
 	public Embargo(){
 		super("Embargo", 2, 0, 0,
 				new CardType[] {CardType.Action});
 	}
+	public void Action(ArrayList<Player> players,
+			Player currentPlayer, Board board){
+		currentPlayer.addMoney(2);
+		board.addToTrash(this);
+		boolean repeat;
+		Scanner input = new Scanner(System.in);
+		do{
+			repeat = false;
+			System.out.println("Which supply pile would you like to add an embargo to?\n >");
+			String ans = input.nextLine();
+			try{
+				board.getDeck(board.getDeckIdx(ans)).addEmbargos(1);
+			}
+			catch(Exception e){
+				System.out.println("That was not a valid name. Try again!");
+				repeat = true;
+			}
+		}while(repeat);
+		input.close();
+	};
 	
 }
