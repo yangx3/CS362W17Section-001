bd00d4525cf1e78e28be830dddcf95dad424c08e
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-11 11:08:30 -0800

    implemented unit test for action of village

--- projects/rosenber/Dominion/src/test/java/org/cs362/dominion/VillageTest.java
+++ projects/rosenber/Dominion/src/test/java/org/cs362/dominion/VillageTest.java
@@ -1,7 +1,7 @@
 package org.cs362.dominion;
 
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import org.junit.Test;
 
 public class VillageTest {
@@ -19,7 +19,27 @@ public class VillageTest {
 		assertEquals("Village value doesn't match",
 				0, test.getTreasure());
 		assertTrue("Village type doesn't match",
-				test.isCardType(CardType.Action));
+				test.isCardType(CardType.Action));		
+	}
+	
+	@Test
+	public void testAction(){
+		ArrayList<Player> plist = new ArrayList<Player>();
+		Player player = new AIPlayer(1);
+		plist.add(player);
+		Board board = new Board();
+		
+		player.giveCard(new Copper());
+		
+		int handSize = player.numCardsHand();
+		
+		Card c = new Village();
+		c.Action(plist, player, board);
+		assertEquals("Player didn't draw a card",
+				handSize+1, player.numCardsHand());
+		assertEquals("Player didn't gain 2 actions",
+				player.getActions(), 2);
+		
 		
 	}
 }
