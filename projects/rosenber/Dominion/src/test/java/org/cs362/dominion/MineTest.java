65d31de299332be64d4581c03c85c6bfae7cc91a
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-11 11:19:07 -0800

    implemented unit test for action of mine

--- projects/rosenber/Dominion/src/test/java/org/cs362/dominion/MineTest.java
+++ projects/rosenber/Dominion/src/test/java/org/cs362/dominion/MineTest.java
@@ -1,7 +1,7 @@
 package org.cs362.dominion;
 
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import org.junit.Test;
 
 public class MineTest {
@@ -20,7 +20,51 @@ public class MineTest {
 				0, test.getTreasure());
 		assertTrue("Mine type doesn't match",
 				test.isCardType(CardType.Action));
-		
 	}
 
+	@Test
+	public void testAction(){
+		ArrayList<Player> players = new ArrayList<Player>();
+		Player currentPlayer = new AIPlayer(1);
+		Board board = new Board();
+		
+		currentPlayer.giveCard(new Copper());
+		currentPlayer.drawCard();
+		board.createDeck(new Silver(), 1);
+		
+		int trash = board.numCardsInDeck(0);
+		
+		Card c = new Mine();
+		c.Action(players, currentPlayer, board);
+		currentPlayer.drawCard();
+		
+		assertEquals("Trash didn't gain card",
+				trash+1, board.numCardsInDeck(0));
+		
+		try{
+			currentPlayer.playCard("Silver");
+		}
+		catch(Exception e){
+			fail("Player's hand didn't contain a siler");
+		}
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
