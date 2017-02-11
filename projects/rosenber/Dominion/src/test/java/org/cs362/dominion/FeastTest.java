7f4c8184ffb6358425599e1383232d15502ac668
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-11 11:25:28 -0800

    implemented unit test for action of feast

--- projects/rosenber/Dominion/src/test/java/org/cs362/dominion/FeastTest.java
+++ projects/rosenber/Dominion/src/test/java/org/cs362/dominion/FeastTest.java
@@ -1,7 +1,7 @@
 package org.cs362.dominion;
 
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import org.junit.Test;
 
 public class FeastTest {
@@ -23,4 +23,26 @@ public class FeastTest {
 		
 	}
 
+	@Test
+	public void testAction(){
+		ArrayList<Player> players = new ArrayList<Player>();
+		Player currentPlayer = new AIPlayer(1);
+		Board board = new Board();
+		board.createDeck(new Copper(), 5);
+		
+		Card c = new Feast();
+		
+		c.Action(players, currentPlayer, board);
+		currentPlayer.drawCard();
+		
+		assertEquals("Trash didn't gain the card",
+				1, board.numCardsInDeck(0));
+		try{
+			currentPlayer.playCard("Copper");
+		}
+		catch(Exception e){
+			fail("Player doesn't have a copper");
+		}
+	}
+	
 }
