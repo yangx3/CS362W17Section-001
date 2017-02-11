120a2c0a7abb068b600f9dfffb768fdfa387a88f
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-10 23:20:22 -0800

    implemented action for baron

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Baron.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Baron.java
@@ -1,10 +1,32 @@
 package org.cs362.dominion;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Baron extends Card{
 
 	public Baron(){
 		super("Baron", 4, 0, 0,
 				new CardType[] {CardType.Action});
 	}
-	
+	public void Action(ArrayList<Player> players,
+			Player currentPlayer, Board board){
+		currentPlayer.addBuys(1);
+		Scanner input = new Scanner(System.in);
+		System.out.println("Discard an estate for +4 coins? If no, gain an estate\n(y/n)>");
+		String ans = input.nextLine();
+		if(ans == "y"){
+			try{
+				currentPlayer.discardCard("Estate");
+				currentPlayer.addMoney(4);
+			}
+			catch(Exception e){
+				System.out.println("You had no estate to discard, you gain one instead!");
+				currentPlayer.giveCard(board.draw(board.getDeckIdx("Estate")));
+			}
+		}
+		else
+			currentPlayer.giveCard(board.draw(board.getDeckIdx("Estate")));	
+		input.close();
+	};
 }
