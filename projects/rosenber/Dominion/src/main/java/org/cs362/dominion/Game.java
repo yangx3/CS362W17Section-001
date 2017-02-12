b8dbd5f2c4eae44ab620bb2418243da65ad556fd
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-12 09:24:02 -0800

    fixed declare winner function

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Game.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Game.java
@@ -126,11 +126,13 @@ public class Game {
 	
 	//declare winner
 	public void declareWinner(){
-		int maxVP = -1;
+		double maxVP = (Double.POSITIVE_INFINITY * -1);
 		Player winner = null;
 		for(int j=0; j<players.size(); j++)
-			if(players.get(j).getVictoryPoints() > maxVP)
+			if(players.get(j).getVictoryPoints() > maxVP){
 				winner = players.get(j);
+				maxVP = winner.getVictoryPoints();
+			}
 		System.out.println("The game is over!\n"
 				+ winner.getName() + " is the winner with "
 				+ maxVP + " victory points!!");
