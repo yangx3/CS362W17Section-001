6d6cbc02aa45049ddbae5d8a865e8335a8b5a1ec
TheEnderPrime <dbaugh.oregon@gmail.com>
2017-02-12 11:00:35 -0800

    sorry for all the weird files... trying something

--- projects/baughd/src/test/java/PlayerTest.java
+++ projects/baughd/src/test/java/PlayerTest.java
@@ -42,19 +42,56 @@ public class PlayerTest {
 
     @Test
     public void testInitializePlayerTurn() {
-
+        assertEquals(player1.hand.size(), 0);
+        assertEquals(player1.deck.size(), 0);
+        assertEquals(player1.discard.size(), 10);
+        assertEquals(player1.playedCards.size(), 0);
+        assertEquals(player1.numActions, 0);
+        assertEquals(player1.coins, 0);
+        assertEquals(player1.numBuys, 0);
+        System.out.println(player1);
+        player1.initializePlayerTurn();
+        assertEquals(player1.hand.size(), 5);
+        assertEquals(player1.deck.size(), 5);
+        assertEquals(player1.discard.size(), 0);
+        assertEquals(player1.playedCards.size(), 0);
+        assertEquals(player1.numActions, 1);
+        assertEquals(player1.coins, 0);
+        assertEquals(player1.numBuys, 1);
+        System.out.println(player1);
     }
 
     @Test
     //card goes in discard,
     public void testGain() {
-
+        assertEquals(player1.hand.size(), 0);
+        assertEquals(player1.deck.size(), 0);
+        assertEquals(player1.discard.size(), 10);
+        assertEquals(player1.playedCards.size(), 0);
+        System.out.println(player1);
+        player1.gain(Card.getCard(cards, Card.CardName.Province));
+        assertEquals(player1.hand.size(), 0);
+        assertEquals(player1.deck.size(), 0);
+        assertEquals(player1.discard.size(), 11);
+        assertEquals(player1.playedCards.size(), 0);
+        System.out.println(player1);
     }
 
     @Test
         //Discard hand
     public void testDiscard() {
-
+        player1.initializePlayerTurn();
+        assertEquals(player1.hand.size(), 5);
+        assertEquals(player1.deck.size(), 5);
+        assertEquals(player1.discard.size(), 0);
+        assertEquals(player1.playedCards.size(), 0);
+        System.out.println(player1);
+        player1.discard(Card.getCard(player1.hand, Card.CardName.Copper));
+        assertEquals(player1.hand.size(), 4);
+        assertEquals(player1.deck.size(), 5);
+        assertEquals(player1.discard.size(), 1);
+        assertEquals(player1.playedCards.size(), 0);
+        System.out.println(player1);
     }
 
     @Test
