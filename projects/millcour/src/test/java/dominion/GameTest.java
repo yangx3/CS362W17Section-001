5daffa9801be0d12afbe66327b17a9cf5fca067c
Courtney Miller <Courtney Miller>
2017-02-13 15:29:40 -0800

    refined testing function for Smithy

--- projects/millcour/src/test/java/dominion/GameTest.java
+++ projects/millcour/src/test/java/dominion/GameTest.java
@@ -3,32 +3,42 @@ package dominion;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 
 public class GameTest {
+	
+	private Game game;
+	
+	@Before
+	public void setup(){
+		this.game = new Game();
+		this.game.setGame();
+	}
 
+	@After
+	public void tearDown(){
+		//this.game.close();
+		this.game = null;
+	}
+	
 	@Test
 	public void playSmithy(){
-		Game game = new Game();
-		game.setGame();
-		game.getPlayers().get(0).getDraw().getPile().remove(0);
-		game.getPlayers().get(0).getDraw().getPile().remove(0);
-		game.getPlayers().get(0).getDraw().getPile().remove(0);
-		game.getPlayers().get(0).getDraw().getPile().remove(0);
-		game.getPlayers().get(0).getDraw().getPile().remove(0);
-		game.getPlayers().get(0).getDraw().getPile().remove(0);
-		game.getPlayers().get(0).getDraw().getPile().remove(0);
-		game.getPlayers().get(0).getDraw().getPile().remove(0);
-		game.getPlayers().get(0).getHand().getPile().add(new Card(Card.CardName.Smithy, Card.Type.ACTION, 4, 0, 0));
-		game.getPlayers().get(0).getHand().getPile().add(new Card(Card.CardName.Smithy, Card.Type.ACTION, 4, 0, 0));
-		game.invokeAction(0);
+		game.runAction(Card.CardName.Smithy, 0);
+		System.out.println("HERE: " + game.getPlayers().get(0).getDraw().getPile().size());
+		System.out.println("HERE: " + game.getPlayers().get(0).getHand().getPile().size());
+		game.getPlayers().get(0).getHand().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 0));
 		assertEquals(game.getPlayers().get(0).getHand().getPile().size(), 4);
-		
+		for(int i=game.getPlayers().get(0).getDraw().getPile().size()-1; i>=0; i--){
+			game.getPlayers().get(0).getDraw().getPile().remove(i);
+			game.getPlayers().get(0).getDiscard().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 0));
+		}
+		game.runAction(Card.CardName.Smithy, 0);
+		assertEquals(game.getPlayers().get(0).getHand().getPile().size(), 7);
 	}
 
 	@Test
 	public void playVillage(){
-		Game game = new Game();
-		game.setGame();
 		while(game.getPlayers().get(0).getDraw().getPile().size() > 0){
 			game.getPlayers().get(0).getDraw().getPile().remove(0);
 		}
@@ -48,8 +58,6 @@ public class GameTest {
 	
 	@Test
 	public void playTribute(){
-		Game game = new Game();
-		game.setGame();
 		game.getPlayers().get(1).getDiscard().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 1));
 		while(game.getPlayers().get(1).getDraw().getPile().size() > 0){
 			game.getPlayers().get(1).getDraw().getPile().remove(0);
@@ -59,8 +67,8 @@ public class GameTest {
 		int compare2 = game.getPlayers().get(0).getDraw().getPile().size();
 		assertEquals(compare, compare2);
 		game.getPlayers().get(0).getDiscard().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 1));
-		while(game.getPlayers().get(0).getDraw().getPile().size() > 0){
-			game.getPlayers().get(0).getDraw().getPile().remove(0);
+		for(int i=game.getPlayers().get(0).getDraw().getPile().size()-1; i>=0; i--){
+			game.getPlayers().get(0).getDraw().getPile().remove(i);
 		}
 		compare = game.getPlayers().get(1).getDraw().getPile().size();
 		game.runAction(Card.CardName.Tribute, 1);
@@ -70,8 +78,6 @@ public class GameTest {
 	
 	@Test
 	public void playMine(){
-		Game game = new Game();
-		game.setGame();
 		game.getPlayers().get(0).getHand().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 1));
 		game.getPlayers().get(0).getHand().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 1));
 		game.runAction(Card.CardName.Mine, 0);
@@ -91,8 +97,6 @@ public class GameTest {
 	
 	@Test
 	public void playGreatHall(){
-		Game game = new Game();
-		game.setGame();
 		int compare = game.getPlayers().get(0).getHand().getPile().size();
 		int compare2 = game.getPlayers().get(0).getNumActions();
 		game.runAction(Card.CardName.GreatHall, 0);
@@ -111,8 +115,6 @@ public class GameTest {
 	
 	@Test
 	public void playFeast(){
-		Game game = new Game();
-		game.setGame();
 		game.getPlayers().get(0).getDiscard().getPile().add(new Card(Card.CardName.Feast, Card.Type.ACTION, 0, 0, 0));
 		int compare = game.getPlayers().get(0).getDiscard().getPile().size();
 		game.runAction(Card.CardName.Feast, 0);
@@ -122,8 +124,6 @@ public class GameTest {
 	
 	@Test
 	public void playEmbargo(){
-		Game game = new Game();
-		game.setGame();
 		game.runAction(Card.CardName.Embargo, 0);
 		assertEquals(game.getPlayers().get(0).getCoins(), 2);
 		game.getPlayers().get(0).getDiscard().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 1));
@@ -139,8 +139,6 @@ public class GameTest {
 	
 	@Test
 	public void playCutpurse(){
-		Game game = new Game();
-		game.setGame();
 		int numCopper = 0;
 		game.getPlayers().get(0).getHand().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 0));
 		game.runAction(Card.CardName.Cutpurse, 0);
@@ -157,8 +155,6 @@ public class GameTest {
 	
 	@Test
 	public void playCouncilRoom(){
-		Game game = new Game();
-		game.setGame();
 		int compare;
 		int compare2;
 		game.getPlayers().get(0).getDiscard().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 0));
@@ -177,8 +173,6 @@ public class GameTest {
 	
 	@Test
 	public void playBaron(){
-		Game game = new Game();
-		game.createPlayers();
 		int numEstate = 0;
 		game.getPlayers().get(0).setNumBuys(0);
 		game.getPlayers().get(0).getHand().getPile().add(new Card(Card.CardName.Estate, Card.Type.VICTORY, 0, 0, 0));
@@ -195,8 +189,6 @@ public class GameTest {
 	
 	@Test
 	public void playAmbassador(){
-		Game game = new Game();
-		game.setGame();
 		for(int i=0; i<game.getPlayers().get(0).getHand().getPile().size(); i++){
 			game.getPlayers().get(0).getHand().getPile().remove(0);
 		}
@@ -208,29 +200,32 @@ public class GameTest {
 	
 	@Test
 	public void playAdventurer(){
-		Game game = new Game();
-		game.setGame();
 		game.getPlayers().get(0).getDiscard().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 0));
 		game.getPlayers().get(0).getDiscard().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 0));
 		game.getPlayers().get(0).getDiscard().getPile().add(new Card(Card.CardName.Copper, Card.Type.TREASURE, 0, 0, 0));
 		game.runAction(Card.CardName.Adventurer, 0);
 		assertEquals(game.getPlayers().get(0).getHand().getPile().size(), 2);
-		for(int i=0; i<game.getPlayers().get(0).getDraw().getPile().size(); i++){
-			game.getPlayers().get(0).getDraw().getPile().remove(0);
+		for(int i=game.getPlayers().get(0).getDraw().getPile().size()-1; i>=0; i--){
+			game.getPlayers().get(0).getDraw().getPile().remove(i);
 		}
 		game.runAction(Card.CardName.Adventurer, 0);
 	}
 	
 	@Test
 	public void checkingForEnd(){
-		Game game = new Game();
-		game.setGame();
-		System.out.println("SIZE: " + game.getBoard().get(2).getPile().size());
-		for(int i=game.getBoard().get(2).getPile().size()-1; i<=0 ; i--){
-			System.out.println("REMOVING");
+		for(int i=game.getBoard().get(2).getPile().size()-1; i>=0 ; i--){
 			game.getBoard().get(2).getPile().remove(i);
 		}
 		assertEquals(game.getBoard().get(2).getPile().size(), 0);
+		assertEquals(game.checkForEnd(), false);
+		for(int i=9; i>=0; i--){
+			game.getBoard().get(13).getPile().remove(i);
+			game.getBoard().get(14).getPile().remove(i);
+			game.getBoard().get(15).getPile().remove(i);
+			game.getBoard().get(2).getPile().add(new Card(Card.CardName.Province, Card.Type.VICTORY, 0, 0, 0));
+		}
+		assertEquals(game.checkForEnd(), false);
+		game.fillActionCards();
 		assertEquals(game.checkForEnd(), true);
 	}
 }
