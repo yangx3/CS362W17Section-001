df697941c11306fea2a27d9be3f66363ecc01315
Robert Rosenberger <rosenber@oregonstate.edu>
2017-02-13 01:46:25 -0800

    modified more whitespace

--- projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Garden.java
+++ projects/rosenber/Dominion/src/main/java/org/cs362/dominion/Garden.java
@@ -7,10 +7,5 @@ public class Garden extends Card{
 		super("Garden", 4, 0, 1,
 				new CardType[] {CardType.Victory});
 		
-	}
-
-//	public int getVictoryPoints(Player player){
-//		return player.numTotalCards()/10;
-//	}
-	
+	}	
 }
