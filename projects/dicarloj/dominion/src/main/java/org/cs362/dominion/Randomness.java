69b4ed7ec626b7aca853f23321b41d38c58fa20c
Jonathan Di Carlo <dicarloj@oregonstate.edu>
2017-02-14 00:50:55 -0800

    final

---
+++ projects/dicarloj/dominion/src/main/java/org/cs362/dominion/Randomness.java
@@ -0,0 +1,52 @@
+package org.cs362.dominion;
+
+import java.util.List;
+import java.util.Random;
+
+
+
+
+public final class Randomness {
+
+  private Randomness(){
+    //no instances
+  }
+  public static final long SEED = 0;
+
+  /**
+   * The random number used any testtime a random choice is made. 
+   */
+ public static Random random = new Random();
+
+  public static void reset(long newSeed){
+    random = new Random(newSeed);
+  }
+
+  public static int totalCallsToRandom = 0;
+
+ 
+  /**
+   * Uniformly random int from [0, i)
+   */
+  public static int nextRandomInt(int i) {
+    totalCallsToRandom++;
+    return random.nextInt(i);
+  }
+  public static <T> T randomMember(List<T> list) {
+    if (list == null || list.isEmpty())
+      throw new IllegalArgumentException("Expected non-empty list");
+    return list.get(nextRandomInt(list.size()));
+  }
+
+	public  static void main(String args[]){
+		
+//		Randomness.reset(10);	   
+
+		for(int i=0; i<10;i++){
+			System.out.println("value=: "+ Randomness.nextRandomInt(10));
+		}
+		System.exit(0);   
+	}
+
+
+}
